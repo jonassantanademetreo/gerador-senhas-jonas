8f80e57084347659be10d7b9343768a10ac35ecb
jonassantanademetreo <145785837+jonassantanademetreo@users.noreply.github.com>
2025-08-12 08:52:39 -0300

    jj

---
+++ main.js
@@ -0,0 +1,8 @@
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
+
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+
+botoes[0].onclick =n diminuiTamanho;
+botoes[1]
