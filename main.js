993b451bebc14783f9d44cb9a073296badce70ea
jonassantanademetreo <145785837+jonassantanademetreo@users.noreply.github.com>
2025-09-02 07:55:06 -0300

    jonas

--- main.js
+++ main.js
@@ -4,5 +4,41 @@ numeroSenha.textContent = tamanhoSenha;
 
 const botoes = document.querySelectorAll('.parametro-senha__botao');
 
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
+
+function diminuiTamanho(){
+    if(tamanhoSenha > 1){
+        tamanhoSenha = tamanhoSenha - 1;
+    }
+    numeroSenha.textContent=tamanhoSenha;
+    geraSenha();
+}
 botoes[0].onclick =n diminuiTamanho;
 botoes[1]
+
+function aumentaTamanho(){
+    if(tamanhoSenha < 20){
+        tamanhoSenha = tamanhoSenha + 1;
+    }
+    numeroSenha.textContent=tamanhoSenha
+    geraSenha();
+}
+
+const campoSenha = document.querySelector('#campo-senha');
+
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
+const numeros = '0123456789';
+const simbolo = '@#?!*+%&°ª§+='
+geraSenha();
+
+function geraSenha(){
+    let senha = '';
+    for (let i=0; i<tamanhoSenha; i++) {
+        let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+        numeroAleatorio = Math.floor(numeroAleatorio);
+        senha = senha + letrasMaiusculas[numeroAleatorio]
+    }
+    campoSenha.value = senha;
+}
