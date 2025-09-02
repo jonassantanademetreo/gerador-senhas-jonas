12ff97f784ac29e93a86dfe65fb0107df71218d1
jonassantanademetreo <145785837+jonassantanademetreo@users.noreply.github.com>
2025-09-02 08:31:46 -0300

    jonas

--- main.js
+++ main.js
@@ -2,7 +2,14 @@ const numeroSenha = document.querySelector('.parametro-senha__texto');
 let tamanhoSenha = 12;
 numeroSenha.textContent = tamanhoSenha;
 
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
+const numeros = '0123456789';
+const simbolo = '@#?!*+%&°ª§+='
 const botoes = document.querySelectorAll('.parametro-senha__botao');
+const campoSenha = document.querySelector('#campo-senha');
+const checkbox = document.querySelector('#checkbox');
+const forcaSenha = document.querySelector('#fraca');
 
 botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentaTamanho;
@@ -25,15 +32,11 @@ function aumentaTamanho(){
     geraSenha();
 }
 
-const campoSenha = document.querySelector('#campo-senha');
-const checkbox = document.querySelector('#checkbox');
+
 
 for (i = 0;i <checkbox.length) 
 
-const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
-const numeros = '0123456789';
-const simbolo = '@#?!*+%&°ª§+='
+
 geraSenha();
 
 function geraSenha(){
@@ -45,3 +48,4 @@ function geraSenha(){
     }
     campoSenha.value = senha;
 }
+
