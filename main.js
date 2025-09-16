bad3768b54fc531b3d441ced9a4480e6acec0219
jonassantanademetreo <145785837+jonassantanademetreo@users.noreply.github.com>
2025-09-16 11:29:46 -0300

    jbg

--- main.js
+++ main.js
@@ -61,7 +61,7 @@ function geraSenha() {
         senha = senha + alfabeto[numeroAleatorio];
     }
     campoSenha.value = senha;
-    classificaSenha();
+    classificaSenha(alfabeto.length);
 }
 
 function classificaSenha() {
@@ -69,12 +69,16 @@ function classificaSenha() {
     let entropia = tamanhoSenha * Math.log2(tamanhoSenha);
 
     forcaSenha.classList.remove('fraca', 'media', 'forte');
-    if (tamanhoSenha > 11) {
+    if (entropia > 57) {
         forcaSenha.classList.add('forte');
-    } else if (tamanhoSenha > 5 && tamanhoSenha < 12) {
+    } else if (entropia > 35 && entropia < 57) {
         forcaSenha.classList.add('media');
-    } else if (tamanhoSenha <= 5) {
+    } else if (entropia <= 35) {
         forcaSenha.classList.add('fraca');
     }
+
+    const valorEntropia = document.querySelector('.entropia')
+valorEntropia.textContent = "Um conputador pode levar até " +
+Math.
 
 }
